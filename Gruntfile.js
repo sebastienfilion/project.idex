1902f20d7070c7ddbdb94fe0d67c0866c1514bff
Sebastien Filion <sebastienfilion@mac.com>
2014-02-02 14:40:52 -0500

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+
+    var copyrights, banner, footer;
+
+    copyrights = '/* http://idesignexperiences.com */\n\n';
+
+    banner = '(function(idex, window, document, undefined) {';
+    footer = '}(window.idex = window.idex || {}. window, document));';
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                banner: banner,
+                footer: footer
+            },
+            dist: {
+                src: ['source/scripts/*.js'],
+                dest: 'source/scripts/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: copyrights
+            },
+            dist: {
+                files: {
+                    'assets/scripts/<%= pkg.name %>.min.js': ['source/scripts/<%= pkg.name %>.js']
+                }
+            }
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'source/scripts/*.js'],
+            options: {
+                globals: {
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
+        },
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'source/sass',
+                    cssDir: 'assets/stylesheets'
+                }
+            }
+        },
+        watch: {
+            files: ['source/**'],
+            tasks: ['jshint', 'concat', 'uglify', 'compass']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'compass']);
+};
